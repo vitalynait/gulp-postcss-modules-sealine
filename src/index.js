89f57e811de8ca502b81d0fae7648d6881885886
Vitaly Nait <vitnait@mail.ru>
2018-03-13 13:35:14 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,103 @@
+/*
+===============================================================================
+  Gulp Vue-Split
+  Copyright 2016 Sebastian Software GmbH <https://www.sebastian-software.de>
+===============================================================================
+*/
+
+import through from "through2"
+import File from "vinyl"
+import postcss from "postcss"
+import postcssModules from "postcss-modules"
+import series from "async/series"
+import pathModule from "path"
+import crc from "crc"
+import gutil from "gulp-util"
+
+var memCache = {}
+
+export function generateHash(filename)
+{
+  const relPath = pathModule.relative(process.cwd(), filename)
+  return crc.crc32(relPath).toString(16)
+}
+
+export function generateScopedName(name, filename)
+{
+  return `${name}-SealineTravel`
+}
+
+export default function postcssModulesPlugin(plugins = [], config)
+{
+  var moduleMapping = null
+
+  function processStyle(done, text, path, base)
+  {
+    if (!text)
+      return done()
+
+    return postcss([
+      ...plugins,
+      postcssModules({
+        generateScopedName: generateScopedName,
+        getJSON: function(cssFileName, json)
+        {
+          moduleMapping = JSON.stringify(json)
+        }
+      })
+    ])
+    .process(text, {
+      from: path
+    })
+    .then((result) =>
+    {
+      if (memCache[path] === moduleMapping)
+        return done()
+
+      memCache[path] = moduleMapping
+
+      const fileObj = new File({
+        contents: new Buffer(moduleMapping),
+        path: path.replace(".scss", ".scss.json"),
+        base: base
+      })
+
+      return done(null, fileObj)
+    })
+  }
+
+
+  function transform(file, encoding, callback)
+  {
+    /* eslint no-invalid-this: 0 */
+
+    if (file.isNull())
+      return callback(null, file)
+
+    if (file.isStream())
+      return callback(new gutil.PluginError("gulp-postcss-modules", "Streams are not supported"))
+
+    var self = this
+
+    var filePath = file.path
+    var fileBase = file.base
+    var fileContent = file.contents.toString("utf8")
+
+    return series([
+      function(done)
+      {
+        processStyle(done, fileContent, filePath, fileBase)
+      }
+    ],
+    (err, results) =>
+    {
+      if (err)
+        return callback(new gutil.PluginError("gulp-postcss-modules", err))
+
+      results.forEach((resultFile) => resultFile && self.push(resultFile))
+      return callback()
+    })
+  }
+
+  return through.obj(transform)
+}
